7e6138b9f7a2cfdb1ea9fc9831050b097bf4d711
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2024-11-26 01:44:01 +0600

    delete data remove from ui

--- src/User.jsx
+++ src/User.jsx
@@ -1,15 +1,22 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const User = () => {
-    const users = useLoaderData();
+    const loaderUsers = useLoaderData();
+    const [users , setUsers]= useState(loaderUsers)
     const handelDeleteUser = id => {
         console.log('delete id ', id);
         fetch(`http://localhost:5000/user/${id}`, {
             method: 'DELETE'
         }).then(res => res.json())
         .then(data =>{
-            alert('user data delete successfully');
-            console.log(data);
+            if(data.deletedCount > 0){
+                alert('user data delete successfully');
+
+            }
+            const reaminig = users.filter(item => item._id!==id);
+            setUsers(reaminig)
+            
         })
     }
     return (
